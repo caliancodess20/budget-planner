f837178111c4db72b5f56790c62586e3e5dc8ae6
SANSKRUTI PRASHANT CHANEKAR <chanekar.25bai10603@vitbhopal.ac.in>
2026-09-15 15:29:20 +0530

    Add Income class to handle income transactions

---
+++ Income.java
@@ -0,0 +1,12 @@
+public class Income extends Transaction {
+
+    public Income(String description, double amount) {
+        super(description, amount);
+    }
+
+    @Override
+    public void displayTransaction() {
+        System.out.println("Income: " + getDescription());
+        System.out.println("Amount: ₹" + getAmount());
+    }
+}
